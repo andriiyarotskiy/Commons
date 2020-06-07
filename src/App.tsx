0195081ad4818c723f4b17723a86f6bf8f2f5f16
Andriy <refwol131@mail.ru>
2020-06-08 02:14:21 +0300

    #2_home_work todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Todo from "./components/Todolist";
 
-function App() {
+const App = () => {
   return (
     <div>
-        dev
+        <Todo />
     </div>
   );
 }
