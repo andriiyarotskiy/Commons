26ce81a11de79b28da031076a7cafdab17817548
Andriy <refwol131@mail.ru>
2020-06-11 23:02:48 +0300

    'без_рефакторинга'

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,28 @@ import Todo from "./components/Todolist";
 import InputTask from "./components/InputTask/InputTask";
 import {v1} from "uuid";
 
+
+export type ArrType = {
+    name: string
+    id: string
+}
+
 const App = () => {
     const [arr, setArr] = useState([]) // хук для данных newObj
 
+
     let addName = (name: string) => {
         let newObj = {name: name, id: v1()}
-        let newArr: any = [...arr, newObj]
+        let newArr: any = [...arr, newObj]  // <= хз как без any
         setArr(newArr)
     }
 
-  return (
-    <div>
-        <Todo />
-        <InputTask arr={arr} addName={addName}/>
-    </div>
-  );
+    return (
+        <div>
+            {/*<Todo />*/}
+            <InputTask arr={arr} addName={addName}/>
+        </div>
+    );
 }
 
 export default App;
--- src/components/InputTask/InputTask.tsx
+++ src/components/InputTask/InputTask.tsx
@@ -1,29 +1,34 @@
 import React, {useState} from "react";
+import {ArrType} from "../../App";
 
 
-const InputTask = (props: any) => {
+type InputTaskType = {
+    arr: Array<ArrType>
+    addName: (name: string) => void
+}
+
+const InputTask = (props: InputTaskType) => {
 
     const [name, setName] = useState('') // Хук для инпута
 
     function sayHello(name: string) {
-        if (name) {
-            alert("Hello " + name)
-        }
+        if (name) alert("Hello " + name)
     }
 
     return (
         <div>
-            <input value={name}
-                   onChange={(e) => {
-                       setName(e.currentTarget.value)
-                   }}
-                   onKeyPress={(e) => {
-                       if (e.charCode === 13) {
-                           props.addName(name)
-                           sayHello(name)
-                           setName('')
-                       }
-                   }}
+            <input
+                value={name}
+                onChange={(e) => {
+                    setName(e.currentTarget.value)
+                }}
+                onKeyPress={(e) => {
+                    if (e.charCode === 13) {
+                        props.addName(name)
+                        sayHello(name)
+                        setName('') // Очищаю инпут после ввода
+                    }
+                }}
             />
             <button onClick={(e) => {
                 props.addName(name)
