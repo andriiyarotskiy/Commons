f2bb89efdbafaf1c6f545d3ad62c67770d221bcc
Andriy <refwol131@mail.ru>
2020-06-20 20:31:29 +0300

    4 таска, убрал ненужные функции

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,34 @@
 import React, {useState} from 'react';
 import './App.css';
 import InputNya from "./InputNya/InputNya";
+import ButtonNya from "./ButtonNya/ButtonNya";
 
 
 function App() {
 
     let [error, setError] = useState<string>('')
 
-    const onEnter = (title: string) => {
-        if (title.trim() !== '') {
-            alert('Hello ' + title.trim())
-        } else {
-            setError('Please add text to the form')
-        }
+
+    const onEnterTest = () => {
+        alert('Say hello')
     }
 
-    const clearInputAfterPress = (title: string) => {
+
+    /* const clearInputAfterPress = (title: string) => {
         if (!title) setError('')
-    }
+    }*/
+    // const onEnter = (title: string) => {
+    //     if (title.trim() !== '') {
+    //         alert('Hello ' + title.trim())
+    //     } else {
+    //         setError('Please add text to the form')
+    //     }
+    // }
 
     return (
         <div>
-            <InputNya onEnter={onEnter}
-                      error={error}
-                      clearInputAfterPress={clearInputAfterPress}/>
+            <InputNya onEnter={onEnterTest} error={error}/>
+            <ButtonNya nameBtn={"+"}/>
         </div>
     );
 }
