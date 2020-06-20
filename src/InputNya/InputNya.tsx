b398e117f5af11052b10d04fd0d50d6c17c3b828
Andriy <refwol131@mail.ru>
2020-06-20 20:48:29 +0300

    fix 4 tasks

--- src/InputNya/InputNya.tsx
+++ src/InputNya/InputNya.tsx
@@ -1,55 +1,36 @@
-import React, {ChangeEvent, KeyboardEvent, DetailedHTMLProps, InputHTMLAttributes, useState} from "react";
+import React, {KeyboardEvent, DetailedHTMLProps, InputHTMLAttributes} from "react";
 import s from './InputNya.module.css'
-import ButtonNya from "../ButtonNya/ButtonNya";
 
 
 export type InputNyaPropsType =
     DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> &
     {
-        onEnter?: (title: string) => void,
+        onEnter: () => void,
         error?: string,
-        clearInputAfterPress: (title: string) => void
+        clearInputAfterPress?: () => void
     };
-// добавил title: string чтоб в функции onEnter отображалось value
 
 const InputNya: React.FC<InputNyaPropsType> = ({onEnter, error, ...props}) => {
 
-    let [title, setTitle] = useState('')
-
-    function addInputValue(e: ChangeEvent<HTMLInputElement>) {
-        setTitle(e.currentTarget.value)
-    }
 
     const handleKeyPress = (e: KeyboardEvent) => {
-        props.clearInputAfterPress('') // Вызoв хук из компоненты App чтоб очистить инпут после press
+        props.clearInputAfterPress && props.clearInputAfterPress() // Вызoв хук из компоненты App чтоб очистить инпут после press
         if (e.key === 'Enter') { // Нажатие на Enter запускает функцию OnEnter
-            onEnter && onEnter(title) // вызов функции onEnter из другой компоненты через пропс
-            setTitle('')
+            onEnter && onEnter() // вызов функции onEnter из другой компоненты через пропс
         }
     }
-
-    const handleButtonClick = () => {
-        onEnter && onEnter(title)
-        setTitle('')
-    }
-
 
     return (
         <>
             <div className={s.wrapperInput}>
                 <input className={s.inputNya} {...props}
                        type="text"
-                       value={title}
-                       onChange={addInputValue}
                        onKeyPress={handleKeyPress}
                 />
                 <div className={s.error}>
                     <span>{error}</span>
                 </div>
             </div>
-            <ButtonNya nameBtn={'component button'}
-                       funcClick={handleButtonClick}
-            />
         </>
 
     );
