3ac431f163ad957fefb1155d5e46c2b259a0b303
Andriy <refwol131@mail.ru>
2020-07-04 18:55:58 +0300

    EditableSpan

---
+++ src/common/EditableSpan/EditableSpan.tsx
@@ -0,0 +1,30 @@
+import React, {ChangeEvent, useState} from "react";
+import InputNya from "../InputNya/InputNya";
+
+type EditableSpanType = {
+    value: string
+    changeValueInput: (e: ChangeEvent<HTMLInputElement>) => void
+}
+
+const EditableSpan = (props: EditableSpanType) => {
+
+    let [editMode, setEditMode] = useState(false)
+
+    const onEditMode = () => {
+        setEditMode(true)
+    }
+
+    const offEditMode = () => {
+        setEditMode(false)
+    }
+
+    return (
+        editMode
+            ? <InputNya value={props.value}
+                        onBlur={offEditMode}
+                        onChange={props.changeValueInput}
+            />
+            : <span onClick={onEditMode}>Click on span</span>
+    )
+}
+export default EditableSpan
--- src/components/pages/Junior.tsx
+++ src/components/pages/Junior.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
+import EditableSpan from "../../common/EditableSpan/EditableSpan";
+import s from "./Junior.module.css"
+import ButtonNya from "../../common/ButtonNya/ButtonNya";
+import {saveState, restoreState} from "../../common/EditableSpan/FuncChangeSpan";
 
+type StateType = {
+    x: string
+}
 
 function Junior() {
+
+    const [value, setValue] = useState('')
+
+    function changeValueInput(e: ChangeEvent<HTMLInputElement>) {
+        setValue(e.currentTarget.value)
+    }
+
+    const setStateCallBack = () => {
+        saveState<StateType>("inputValue", {x: value}); // сохранение обьекта
+    }
+
+    const getStateCallBack = () => {
+        const state: StateType = restoreState<StateType>("inputValue", {x: ''});
+        setValue(state.x)
+    }
 
     return (
         <>
             <h1>Junior</h1>
+            <div className={s.middleEditable}>
+                <ButtonNya onClick={setStateCallBack}>SAVE</ButtonNya>
+                <ButtonNya onClick={getStateCallBack}>RESTORE</ButtonNya>
+            </div>
+            <div className={s.middleEditable}>
+                <EditableSpan value={value} changeValueInput={changeValueInput}/>
+            </div>
         </>
     );
 }
