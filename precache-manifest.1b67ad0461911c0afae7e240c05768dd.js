afd94c3fac1f76eb15b9625e33eeec1f79ce93c6
Andriy <refwol131@mail.ru>
2020-08-21 01:49:06 +0300

    Updates

---
+++ precache-manifest.1b67ad0461911c0afae7e240c05768dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40800212a662fe0f2558c261db230e15",
+    "url": "/Home__Task_Ignata/index.html"
+  },
+  {
+    "revision": "d4f70b87d51ac5fa90f9",
+    "url": "/Home__Task_Ignata/static/css/main.96acbba4.chunk.css"
+  },
+  {
+    "revision": "efddf5ec284529a6f0f6",
+    "url": "/Home__Task_Ignata/static/js/2.b80211be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Home__Task_Ignata/static/js/2.b80211be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4f70b87d51ac5fa90f9",
+    "url": "/Home__Task_Ignata/static/js/main.575b6b2a.chunk.js"
+  },
+  {
+    "revision": "3f521796dcfa974ae76c",
+    "url": "/Home__Task_Ignata/static/js/runtime-main.be4aea36.js"
+  },
+  {
+    "revision": "f01061cef4a51ec8b747b03c5a42e2a8",
+    "url": "/Home__Task_Ignata/static/media/preloader3.f01061ce.svg"
+  }
+]);
